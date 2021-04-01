621ea9a83c9f884b4e55556056e8e5dfcc688529
thomasgnuttall <thomasgnuttalll@gmail.com>
2021-04-01 19:43:45 +0200

    skeleton code + stumpy

---
+++ begin.py
@@ -0,0 +1,39 @@
+import stumpy
+
+def DTWMotifDiscovery(TS, subseqlen, maxwarp):
+	"""
+	TODO: Create documentation
+	"""
+    mp, _, _, _ = mpx_v2(TS, subseqlen, subseqlen) # mpx(TS, subseqlen, subseqlen)
+
+    lb_t, sp_rates, pr_rate, prrate, best_so_far, motiffirst, motifsec, DTW_time = dtw_motif(TS, subseqlen, maxwarp, mp)
+
+    
+    totalpairs = len(TS)**2; #(len(TS)-2*subseqlen+1)*(len(TS)-2*subseqlen+2)/2
+
+    pruned_pairs = []
+    for i in range(len(lb_t)):
+        pruned_pairs.append((len(TS)-prrate[i])^2) #(len(TS)-prrate[i]-2*subseqlen+1)*(len(TS)-prrate[i]-2*subseqlen+2)/2
+
+	return best_so_far, motiffirst, motifsec
+
+
+def mpx_v2(timeSeries, minlag, subseqlen):
+	"""
+	TODO: Get documentation from matlab code
+	"""
+	stump_ret = stumpy.stump(timeSeries, m=subseqlen)
+
+	matrixProfile = stump_ret[:, 0]
+	matrixProfileIdx = stump_ret[:, 1]
+	motifsIdx = stump_ret[:, 2]
+	discordsIdx = stump_ret[:, 3]
+
+	return matrixProfile, matrixProfileIdx, motifsIdx, discordsIdx
+
+
+def dtw_motif(TS, subseqlen, maxwarp, mp):
+	"""
+	TODO: Create documentation
+	"""
+	return lb_t, sp_rates, pr_rate, prrate, best_so_far, first_min, sec_min, DTW_time
